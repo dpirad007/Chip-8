c34bc76d488742b8be536ef9eac4de2d5030325c
dpirad007 <dionpinto000@gmail.com>
2022-08-01 11:25:11 +0530

    sdl2 setup

--- chip8_desktop/src/main.rs
+++ chip8_desktop/src/main.rs
@@ -1,6 +1,39 @@
 use chip8_core::*;
+use sdl2::event::Event;
 
 fn main() {
-    let chip8 = Emu::new();
-    println!("Hello, world!");
+    const SCALE: u32 = 15;
+    const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+    const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Chip-8 emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'gameloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'gameloop,
+                _ => {}
+            }
+        }
+    }
 }
